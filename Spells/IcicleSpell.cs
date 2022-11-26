2fc46c6e233ae29822ac7175b2d2c3921d13f459
Martin Matta <martin.matta@softacus.com>
2022-11-26 17:46:23 +0100

    forgot to add the new file

---
+++ Spells/IcicleSpell.cs
@@ -0,0 +1,16 @@
+﻿using MartinMatta_MerlinCore.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinMatta_MerlinCore.Spells
+{
+    public class IcicleSpell : ProjectileSpell
+    {
+        public IcicleSpell(AbstractCharacter caster, int speed) : base(caster, speed)
+        {
+        }
+    }
+}
