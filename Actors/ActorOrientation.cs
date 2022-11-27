e2f09222fafc47e3f48693dc5d24def2fa926939
Martin Matta <martin.matta@softacus.com>
2022-11-27 09:24:33 +0100

    forgot to add the files

---
+++ Actors/ActorOrientation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinMatta_MerlinCore.Actors
+{
+    public enum ActorOrientation
+    {
+        LEFT,
+        RIGHT
+    }
+}
