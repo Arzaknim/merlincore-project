475f58b1a7274809c44a02b791d32b65a6a12a35
Martin Matta <martin.matta@softacus.com>
2022-11-28 15:44:05 +0100

    forgot to add spell effect

---
+++ Spells/AbstractSpellEffect.cs
@@ -0,0 +1,29 @@
+﻿using MartinMatta_MerlinCore.Actors;
+using Merlin2d.Game.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinMatta_MerlinCore.Spells
+{
+    public abstract class AbstractSpellEffect : ICommand
+    {
+        private string name;
+        protected SpellType type;
+        protected AbstractCharacter target;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void SetTarget(AbstractCharacter target)
+        {
+            this.target = target;
+        }
+
+        public abstract void Execute();
+    }
+}
