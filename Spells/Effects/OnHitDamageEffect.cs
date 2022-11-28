dc726f871b394ae160ef118308f391bac84740d5
Martin Matta <martin.matta@softacus.com>
2022-11-28 09:26:00 +0100

    folder for effects with onhitdamgeeffect

---
+++ Spells/Effects/OnHitDamageEffect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinMatta_MerlinCore.Spells.Effects
+{
+    public class OnHitDamageEffect : SpellEffect
+    {
+        private int deltaHP;
+
+        public OnHitDamageEffect(int deltaHP)
+        {
+            this.deltaHP = deltaHP;
+        }
+
+        public override void Execute()
+        {
+            this.target.ChangeHealth(deltaHP);
+        }
+    }
+}
