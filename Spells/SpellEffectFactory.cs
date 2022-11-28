03ebde3e1fe94152f97fc30ec9840b8ba3a22d1a
Martin Matta <martin.matta@softacus.com>
2022-11-28 09:20:05 +0100

    add spelleffect and its factory

---
+++ Spells/SpellEffectFactory.cs
@@ -0,0 +1,21 @@
+﻿using MartinMatta_MerlinCore.Spells.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinMatta_MerlinCore.Spells
+{
+    public class SpellEffectFactory
+    {
+        public SpellEffect CreateEffect(string effectName)
+        {
+            if(effectName == "OnHitDamage")
+            {
+                return new OnHitDamageEffect(-50);
+            }
+            return null;
+        }
+    }
+}
